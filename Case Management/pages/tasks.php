770ce5c65d0113dbb7981be07b05a65b7b6414b8
chrinsley <jchrinsley02@gmail.com>
2026-05-20 12:14:47 +0400

    Update tasks.php

--- Case Management/pages/tasks.php
+++ Case Management/pages/tasks.php
@@ -390,14 +390,6 @@ $html = <<<'HTML'
                             <input type="search" name="q" class="form-control" placeholder="Search cases or tasks…" value="" autocomplete="off" maxlength="200" aria-label="Search">
                         </div>
                     </form>
-                    <ul class="navbar-nav justify-content-end">
-                        <li class="nav-item d-flex align-items-center">
-                            <a href="../pages/lawyer-logout.php" class="nav-link text-white font-weight-bold px-0">
-                                <i class="fa fa-user me-sm-1"></i>
-                                <span class="d-sm-inline d-none">Sign Out</span>
-                            </a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </nav>
