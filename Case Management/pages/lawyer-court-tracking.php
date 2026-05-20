45e3905e44018cdd05b2e8f03f626f1a0067f3df
Gael Rock <gaelrock70@gmail.com>
2026-05-20 12:04:42 +0400

    modif for lawyer-court-tracking.php

--- Case Management/pages/lawyer-court-tracking.php
+++ Case Management/pages/lawyer-court-tracking.php
@@ -347,12 +347,12 @@ NAV;
                         </div>
                     </form>
                     <ul class="navbar-nav justify-content-end">
-                        <li class="nav-item d-flex align-items-center">
+                        <!-- <li class="nav-item d-flex align-items-center">
                             <a href="lawyer-logout.php" class="nav-link text-white font-weight-bold px-0">
                                 <i class="fa fa-user me-sm-1"></i>
                                 <span class="d-sm-inline d-none">Logout</span>
                             </a>
-                        </li>
+                        </li> -->
                     </ul>
                 </div>
             </div>
