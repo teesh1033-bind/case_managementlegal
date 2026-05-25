d0e92137d16cc36967a536021cb9a7eabbb33513
chrinsley <jchrinsley02@gmail.com>
2026-05-25 12:26:51 +0400

    add case-management sql

--- case_management.sql
+++ case_management.sql
@@ -387,7 +387,7 @@ CREATE TABLE `invoices` (
   `notes` text DEFAULT NULL,
   `status` varchar(50) DEFAULT 'unpaid',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `Test` int(255) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
