453e87407cfefd4a976b79775749e700727f26d6
Najaina <najainaliantsoa@gmail.com>
2026-06-10 12:05:23 +0400

    tasks lawyer

--- case_management/inc/client-portal-head.php
+++ case_management/inc/client-portal-head.php
@@ -11,7 +11,9 @@ require_once __DIR__ . '/legalpro-icons.php';
 <link href="../assets/css/app-font-montserrat.css?v=7" rel="stylesheet" />
 <link href="../assets/css/legalpro-portal-shell.css?v=14" rel="stylesheet" />
 <link href="../assets/css/legalpro-client-portal.css?v=21" rel="stylesheet" />
+<?php if (!defined('LEGALPRO_SKIP_DASHBOARD_ENHANCEMENTS')): ?>
 <link href="../assets/css/dashboard-enhancements.css?v=10" rel="stylesheet" />
+<?php endif; ?>
 <link href="../assets/css/legalpro-admin-portal.css?v=14" rel="stylesheet" />
 <link href="../assets/css/legalpro-sidebar-nav.css?v=1" rel="stylesheet" />
 <?php legalpro_icons_asset_links(); ?>
