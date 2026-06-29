ce06ac88d98dee146dc92d8cd4f0f6c85ea014cc
Gael Rock <gaelrock70@gmail.com>
2026-06-29 12:46:41 +0400

    changes in admin about design and in cases

--- inc/admin-portal-head.php
+++ inc/admin-portal-head.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/legalpro-icons.php';
 <?php include __DIR__ . '/portal-theme-head-early.php'; ?>
 <?php legalpro_icons_head_scripts(); ?>
 <link href="../assets/css/legalpro-portal-shell.css?v=21" rel="stylesheet" />
-<link href="../assets/css/legalpro-admin-portal.css?v=35" rel="stylesheet" />
+<link href="../assets/css/legalpro-admin-portal.css?v=37" rel="stylesheet" />
 <link href="../assets/css/dashboard-enhancements.css?v=16" rel="stylesheet" />
 <link href="../assets/css/legalpro-sidebar-nav.css?v=23" rel="stylesheet" />
 <?php legalpro_icons_asset_links(); ?>
